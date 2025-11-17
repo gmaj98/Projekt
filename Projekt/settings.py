86e355b4d41fc0acbf396f912648885fd2427c03
gmaj98 <grzesiekmajewski11@gmail.com>
2025-11-17 21:10:41 +0100

    Add DataBase, create Models, create views in admin panel

--- Projekt/settings.py
+++ Projekt/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ski_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -127,3 +128,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'reactapp', 'build', 'static')
 ]
+
+AUTH_USER_MODEL = 'ski_app.User'
--- ski_app/admin.py
+++ ski_app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import User, Ranking, Tag, Articles, ArticlesComments, Video, VideoComments, Slopes
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Ranking)
+admin.site.register(Tag)
+admin.site.register(Articles)
+admin.site.register(ArticlesComments)
+admin.site.register(Video)
+admin.site.register(VideoComments)
+admin.site.register(Slopes)
+
