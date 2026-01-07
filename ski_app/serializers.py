d5c362e1913169a63cabc02b1bb74d2e6b1a872b
gmaj98 <grzesiekmajewski11@gmail.com>
2026-01-07 20:16:53 +0100

    Add all other serializers

--- ski_app/serializers.py
+++ ski_app/serializers.py
@@ -4,7 +4,7 @@ from .models import User, Ranking, Tag, Articles, ArticlesComments, Video, Video
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ("id", "email", "user_name", "first_name")
 
 
 class RankingSerializer(serializers.ModelSerializer):
@@ -12,4 +12,38 @@ class RankingSerializer(serializers.ModelSerializer):
         model = Ranking
         fields = '__all__'
 
-        
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
+
+
+class ArticlesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Articles
+        fields = '__all__'
+
+
+class ArticlesCommentsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ArticlesComments
+        fields = '__all__'
+
+
+class VideoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Video
+        fields = '__all__'
+
+
+class VideoCommentsSerialzier(serializers.ModelSerializer):
+    class Meta:
+        model = VideoComments
+        fields = '__all__'
+
+
+class SlopesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Slopes
+        fields = '__all__'
